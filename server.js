06ce705affef91587dbcacc025da904086571c87
janmejaybhavsar <89321629+janmejaybhavsar@users.noreply.github.com>
2023-03-26 18:04:53 -0700

    Integrate ChatGPT bot in the application

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
@@ -12,6 +13,32 @@ const { ExpressPeerServer } = require("peer");
 const opinions = {
   debug: true,
 }
+
+const { Configuration, OpenAIApi } = require("openai");
+const config = new Configuration({
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(config);
+
+// Define a function to send a message to the GPT chatbot and return the response
+async function sendMessageToChatbot(message) {
+  if(!message.includes("@ChatGPT")) return;
+  // Remove the "Chat GPT" prefix from the message
+  message = message.replace("@ChatGPT", "");
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Hey give me a response for this : ${message}`,
+    max_tokens: 150,
+    temperature: 0.5,
+    top_p: 1.0,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.5,
+  });
+  console.log(response.data.choices[0].text);
+  return response.data.choices[0].text.trim();
+}
+// sendMessageToChatbot("Hello how are you?");
 
 app.use("/peerjs", ExpressPeerServer(server, opinions));
 app.use(express.static("public"));
@@ -30,8 +57,12 @@ io.on("connection", (socket) => {
     setTimeout(()=>{
       socket.to(roomId).broadcast.emit("user-connected", userId);
     }, 1000)
-    socket.on("message", (message) => {
+    socket.on("message", async (message) => {
       io.to(roomId).emit("createMessage", message, userName);
+      if(message.includes("@ChatGPT")){
+        const response = await sendMessageToChatbot(message); // Send message to the chatbot
+        io.to(roomId).emit("createMessage", response, "Chatbot"); // Send the chatbot's response to the room
+      }
     });
   });
 });
